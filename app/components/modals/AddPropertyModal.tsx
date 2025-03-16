b3a7041dd6b6086c4c3929aa2a82620ae0cb77e7
Abraham <abrahamperezquiroz0@gmail.com>
2025-03-16 17:48:51 -0600

    no se xd

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -12,9 +12,13 @@ import useAddPropertyModal from '@/app/hooks/useAddPropertyModal'
 const AddPropertyModal = () => {
     const [currentStep,setCurrentStep] = useState(1);
     const [dataCategory, setDataCategory] = useState('');
-    const [dataTitle, setDatitle] = useState('');
+    const [dataTitle, setDatatitle] = useState('');
     const [dataDescription,  setDataDescription] = useState('');
-    
+    const [dataPrice, setDataPrice] = useState('');
+    const [dataBedrooms, setDataBedrooms] = useState('');
+    const [dataBathrooms, setDataBathrooms] = useState('');
+    const [dataGuests , setDataGuests] = useState('');
+
     const AddPropertyModal = useAddPropertyModal();
 
     const setCategory = (category: string) => {
@@ -39,7 +43,7 @@ const AddPropertyModal = () => {
                 </>
             ) : currentStep == 2 ?  (
                 <>
-                    <h2 className='mb-6 text-2xl'>Descri be su lugar</h2>
+                    <h2 className='mb-6 text-2xl'>Describe su lugar</h2>
                 
                     <div className='pt-3 pb-6 space-y-4'>
                         <div className='flex flex-col space-y-2'>
@@ -47,7 +51,7 @@ const AddPropertyModal = () => {
                                 <input 
                                     type="text" 
                                     value={dataTitle}
-                                    onChange={(e) => setDatitle(e.target.value)}
+                                    onChange={(e) => setDatatitle(e.target.value)}
                                     className='w-full p-4 border border-gray-600 rounded-xl'
                                 />
                         </div>
@@ -56,7 +60,7 @@ const AddPropertyModal = () => {
                             <label>Description</label>
                                 <textarea
                                     value={dataDescription}
-                                    onChange={(e) => setDatitle(e.target.value)}
+                                    onChange={(e) => setDataDescription(e.target.value)}
                                     className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
                                 />
                         </div>
@@ -78,6 +82,55 @@ const AddPropertyModal = () => {
                 <>
                 <h2 className='mb-6 text-2xl'>Detalles</h2>
 
+                <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Precio por noche</label>
+                                <input 
+                                    type="number" 
+                                    value={dataPrice}
+                                    onChange={(e) => setDataPrice(e.target.value)}
+                                    className='w-full p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+                </div>
+
+                <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Habitaciones</label>
+                                <input 
+                                    type="number" 
+                                    value={dataBedrooms}
+                                    onChange={(e) => setDataBedrooms(e.target.value)}
+                                    className='w-full p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+                </div>
+
+                <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Baños</label>
+                                <input 
+                                    type="number" 
+                                    value={dataBathrooms}
+                                    onChange={(e) => setDataBathrooms(e.target.value)}
+                                    className='w-full p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+                </div>
+
+                <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Número Maximo de Invitados</label>
+                                <input 
+                                    type="number" 
+                                    value={dataGuests}
+                                    onChange={(e) => setDataGuests(e.target.value)}
+                                    className='w-full p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+                </div>
+
+
                     <CustomButton 
                         label='Previous'
                         className='mb-2 bg-black hover:bg-gray-800'
@@ -88,6 +141,26 @@ const AddPropertyModal = () => {
                         label='Next'
                         onClick={() => setCurrentStep(4)}
                     />
+                </>
+            ) : currentStep == 4 ?  (
+                <>
+                    <h2 className='mb-6 text-2xl'>Locación</h2>
+
+                    <div className='pt-3 pb-6 space-y-4'>
+                        Loca loca locación
+                    </div>
+
+                    <CustomButton 
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(3)}
+                    />
+
+                    <CustomButton 
+                        label='Next'
+                        onClick={() => setCurrentStep(5)}
+                    />
+                    
                 </>
             ) : ( 
                 <p>Scorpion</p>
