9305c35a7af609a75b94e731d50ad06466da869b
Abraham <abrahamperezquiroz0@gmail.com>
2025-03-16 17:22:13 -0600

    commit uwu

--- app/components/modals/AddPropertyModal.tsx
+++ app/components/modals/AddPropertyModal.tsx
@@ -2,12 +2,98 @@
 
 import Image from 'next/image'
 
+import { useState } from 'react'
 import Modal from './Modal'
+import CustomButton from '../forms/CustomButton'
+import Categories from '../addproperty/Categories'
 
 import useAddPropertyModal from '@/app/hooks/useAddPropertyModal'
 
 const AddPropertyModal = () => {
+    const [currentStep,setCurrentStep] = useState(1);
+    const [dataCategory, setDataCategory] = useState('');
+    const [dataTitle, setDatitle] = useState('');
+    const [dataDescription,  setDataDescription] = useState('');
+    
     const AddPropertyModal = useAddPropertyModal();
+
+    const setCategory = (category: string) => {
+        setDataCategory(category)
+    }
+
+    const content = (
+        <> 
+            {currentStep == 1 ? (
+                <>
+                    <h2 className='mb-6 text-2xl'>Elegir categoria</h2>
+
+                    <Categories 
+                        dataCategory={dataCategory}
+                        setCategory={(category) => setCategory(category)}
+                    />
+                    
+                    <CustomButton 
+                        label='Next'
+                        onClick={() => setCurrentStep(2)}
+                    />    
+                </>
+            ) : currentStep == 2 ?  (
+                <>
+                    <h2 className='mb-6 text-2xl'>Descri be su lugar</h2>
+                
+                    <div className='pt-3 pb-6 space-y-4'>
+                        <div className='flex flex-col space-y-2'>
+                            <label>Title</label>
+                                <input 
+                                    type="text" 
+                                    value={dataTitle}
+                                    onChange={(e) => setDatitle(e.target.value)}
+                                    className='w-full p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+
+                        <div className='flex flex-col space-y-2'>
+                            <label>Description</label>
+                                <textarea
+                                    value={dataDescription}
+                                    onChange={(e) => setDatitle(e.target.value)}
+                                    className='w-full h-[200px] p-4 border border-gray-600 rounded-xl'
+                                />
+                        </div>
+
+                    </div>
+
+                    <CustomButton 
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(1)}
+                    />
+
+                    <CustomButton 
+                        label='Next'
+                        onClick={() => setCurrentStep(2)}
+                    />
+                </>
+            ) : currentStep == 3 ?  (
+                <>
+                <h2 className='mb-6 text-2xl'>Detalles</h2>
+
+                    <CustomButton 
+                        label='Previous'
+                        className='mb-2 bg-black hover:bg-gray-800'
+                        onClick={() => setCurrentStep(2)}
+                    />
+
+                    <CustomButton 
+                        label='Next'
+                        onClick={() => setCurrentStep(4)}
+                    />
+                </>
+            ) : ( 
+                <p>Scorpion</p>
+            )}
+        </>
+    )
 
     return (
         <>
@@ -15,13 +101,10 @@ const AddPropertyModal = () => {
                 isOpen={AddPropertyModal.isOpen}
                 close={AddPropertyModal.close}
                 label="Agregar property"
-                content={(
-                <p>I</p>
-                )}
+                content={content}
             />
         </>
     )
-
 }
 
 export default AddPropertyModal
--- app/components/navbar/AddPropertyButton.tsx
+++ app/components/navbar/AddPropertyButton.tsx
@@ -4,18 +4,18 @@ import useLoginModal from "@/app/hooks/UseLoginModal"
 import useAddPropertyModal from "@/app/hooks/useAddPropertyModal"
 
 interface AddPropertyButtonProps {
-    userId?: string | null
+    userId?: string | null;
 }
 
 const AddPropertyButton: React.FC<AddPropertyButtonProps> = ({
     userId
 }) => {
     const loginModal = useLoginModal()
-    const AddPropertyModal = useAddPropertyModal( )
+    const addPropertyModal = useAddPropertyModal( )
 
     const airbnbYourHome = () => {
         if (userId) {
-        AddPropertyModal.open()
+            addPropertyModal.open()
         } else {
             loginModal.open()
         }
@@ -23,10 +23,10 @@ const AddPropertyButton: React.FC<AddPropertyButtonProps> = ({
 
     return(
             <div 
-            onClick={airbnbYourHome}
-            className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200"
+                onClick={airbnbYourHome}
+                className="p-2 cursor-pointer text-sm font-semibold rounded-full hover:bg-gray-200"
             >
-                DjangoBnb Our Home
+                Abraham's Djangobnb
             </div>
     )
 }
